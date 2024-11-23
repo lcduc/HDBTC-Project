5e70ce0993125974d8914ce38ebc76e2d0103522
Nthil <ngathidknt@gmail.com>
2024-11-24 03:46:40 +0700

    Hot Fix - CSS file & Button
    - Bootstrap CSS: add Bootstrap CSS files
    - DashBoard.vue: add the button to call predicted data

--- Software/website/src/App.vue
+++ Software/website/src/App.vue
@@ -1,29 +1,20 @@
-<script setup>
-import HelloWorld from './components/LogIn.vue'
+<script>
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
-  <HelloWorld msg="Vite + Vue" />
+  <div id="app container-fluid">
+    <router-view />
+  </div>
 </template>
 
 <style>
 .pagination {
-  /* style for the whole pagination  */
   display: flex;
   justify-content: center;
   text-align: center;
 }
 
 .pagination a {
-  /* style for the anchor */
   color: black;
   border: 1px solid #ddd;
   float: left;
@@ -33,7 +24,6 @@ import HelloWorld from './components/LogIn.vue'
 }
 
 .currentPage a {
-  /* style to indicate the current page  */
   background-color: lightblue;
 }
 
